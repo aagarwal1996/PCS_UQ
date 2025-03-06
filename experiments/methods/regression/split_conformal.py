6a7085315ae65787e8b63e60a474bd84015901df
aagarwal1996 <aa3797@berkeley.edu>
2025-03-05 20:41:32 -0800

    adding split conformal for regression

---
+++ experiments/methods/regression/split_conformal.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+
+class SplitConformal:
+    """
+    Split conformal prediction intervals for regression. 
+
+    Args:
+        model: sklearn regression model
+        alpha: significance level
+        seed: random seed for train-test split
+    """
+    def __init__(self, model, alpha = 0.1, seed = 42):
+
+        self.model = model
+        self.alpha = alpha
+        self.seed = seed
+        self.q = None 
+    
+    def fit(self, X, y):
+        """
+        Fit and calibrate the model 
+
+        Args:
+            X: training features
+            y: training labels
+        """
+        X_train, X_calib, y_train, y_calib = train_test_split(X, y, test_size=0.5, random_state=self.seed)    
+        self.model.fit(X_train, y_train)
+        residuals = np.abs(y_calib - self.model.predict(X_calib))
+        self.q = np.sort(residuals)[int(np.ceil((len(X_calib) + 1) * (1 - self.alpha)) - 1)]
+
+        return self 
+    
+    def predict(self, X):
+        """
+        Predicts intervals for test data using calibration errors.
+
+        Parameters:
+        ----------
+        X : np.ndarray
+            
+        Returns:
+        -------
+        np.ndarray
+            An array of shape (n_samples, 2) containing the lower and upper bounds of the prediction intervals.
+        """
+    
+        # Predict on the test set
+        y_pred_test = self.model.predict(X)
+
+        # Compute prediction intervals
+        lower_bounds = y_pred_test - self.q
+        upper_bounds = y_pred_test + self.q
+
+        return np.column_stack((lower_bounds, upper_bounds))
+
+
+if __name__ == "__main__":
+    X = np.random.randn(100, 10)
+    y = np.random.randn(100)
+    model = RandomForestRegressor()
+    conformal = SplitConformal(model)
+    conformal.fit(X, y)
+    print(conformal.predict(X))
