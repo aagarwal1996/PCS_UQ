231bc095cb934cf9922fc2cf77ada498d002cf2b
aagarwal1996 <aa3797@berkeley.edu>
2025-03-11 09:20:56 -0700

    adjusting temperature scaling for conformal

--- src/conformal_methods/classification/multi_class_conformal.py
+++ src/conformal_methods/classification/multi_class_conformal.py
@@ -8,7 +8,12 @@ from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.base import clone
+from sklearn.datasets import make_classification
 # Local imports
+from src.PCS.classification.calibration_utils import temperature_scaling
+from src.metrics.classification_metrics import get_all_metrics
+
+# Mapie imports
 from mapie.classification import MapieClassifier
 from mapie.conformity_scores import RAPSConformityScore, LACConformityScore, NaiveConformityScore, APSConformityScore 
 
@@ -54,7 +59,12 @@ class MultiClassConformal:
             alpha = self.alpha
         X_train, X_calib, y_train, y_calib = train_test_split(X, y, test_size=0.5, random_state=self.seed)    
         self._train(X_train, y_train)
-        self._calibrate(X_calib, y_calib)
+        self._temperature_scaling(X_calib, y_calib) # Perform temperature scaling on the calibration set
+        self.mapie_classifier = MapieClassifier(estimator=self.model, 
+                                                cv='prefit', 
+                                                conformity_score=self.conf_score,
+                                                random_state=self.seed)
+        self._calibrate(X_calib, y_calib) # Calibrate the model on the calibration set
 
         return self 
     
@@ -65,16 +75,18 @@ class MultiClassConformal:
         Train the model on the training set.
         """
         
+        self.model.fit(X, y)
+        self._trained = True
+    
+    def _temperature_scaling(self, X, y):
+        """
+        Temperature scaling the model on the training set.
+        """
         if self.temperature_scaling:
-            self.model = CalibratedClassifierCV(self.model)
+            self.model = CalibratedClassifierCV(self.model, method='sigmoid', cv = 'prefit')
             self.model.fit(X, y)
         else:
-            self.model.fit(X, y)    
-        self.mapie_classifier = MapieClassifier(estimator=self.model, 
-                                                cv='prefit', 
-                                                conformity_score=self.conf_score,
-                                                random_state=self.seed)
-        self._trained = True
+            self.model = self.model
 
     def _calibrate(self, X, y):
         """
@@ -108,11 +120,11 @@ class MultiClassConformal:
 
 
 if __name__ == "__main__":
-    X = np.random.randn(500, 10)
-    y = np.random.randint(0, 5, 500)
+    X, y = make_classification(n_samples=2000, n_features=10, n_classes=3, n_informative=5)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
     model = RandomForestClassifier()
     conformity_scores = ['RAPS', 'LAC', 'Naive', 'APS']
     for conformity_score in conformity_scores:
-        split_conformal = MultiClassConformal(model, conformity_score=conformity_score)
+        split_conformal = MultiClassConformal(model, conformity_score=conformity_score, temperature_scaling=True)
         split_conformal.fit(X, y)
-        print(split_conformal.predict(X))
+        print(f'Conformity score: {conformity_score}, Metrics: {get_all_metrics(y, split_conformal.predict(X))}')
