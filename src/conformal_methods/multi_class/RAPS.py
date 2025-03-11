0c3adfe3a54ebbec8bc153c6b6700fa769693bd0
aagarwal1996 <aa3797@berkeley.edu>
2025-03-10 18:26:55 -0700

    starting RAPS code

---
+++ src/conformal_methods/multi_class/RAPS.py
@@ -0,0 +1,71 @@
+# General Import
+import numpy as np
+import pandas as pd
+
+# Sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.frozen import FrozenEstimator
+
+
+
+class LAC:
+    def __init__(self, model,alpha = 0.1, cv = 3, random_state = 42, temperature_scaling = True):
+        '''
+        Implementation of the LAC conformal prediction method for multi-class classification.
+
+        Parameters:
+            model: sklearn model (must have a predict_proba method)
+            alpha: significance level
+            cv: number of folds for cross-validation
+            random_state: random state for reproducibility
+        '''
+        self.model = model
+        self.alpha = alpha
+        self.cv = cv
+        self.random_state = random_state
+        self._n_classes = None
+        self._temperature_scaling = temperature_scaling
+    def fit(self, X, y):
+        X_train, X_cal, y_train, y_cal = train_test_split(X, y, test_size=0.5, random_state=self.random_state)
+        self.model.fit(X_train, y_train)
+        self._n_classes = len(np.unique(y))
+        if self._temperature_scaling:
+            self._temperature_scaling(X_cal, y_cal)
+        
+        self.calibrate(X_cal, y_cal)
+   
+    def _temperature_scaling(self, X_cal, y_cal):
+        self.model = FrozenEstimator(self.model)
+        self.model = CalibratedClassifierCV(self.model,cv=self.cv)
+        self.model.fit(X_cal, y_cal)
+
+    def calibrate(self, X_cal, y_cal):
+        class_labels = self.model.classes_
+        class_idx = np.argsort(class_labels)
+        print(class_idx)
+
+if __name__ == "__main__":
+    # Example usage of RAPS for classification
+    from sklearn.datasets import load_iris
+    from sklearn.metrics import accuracy_score
+    from src.metrics.classification_metrics import get_coverage, get_mean_width
+    
+    # Load a sample dataset
+    iris = load_iris()
+    X, y = iris.data, iris.target
+    
+    # Split into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    # Initialize base model and RAPS
+    base_model = RandomForestClassifier(random_state=42)
+    lac = LAC(model=base_model, alpha=0.1)
+    
+    # Fit the model
+    lac.fit(X_train, y_train)
+    
+    
